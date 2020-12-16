660ee65dc195a090e63182ee010c7d6e0384f2a8
said naim <naim.said152@gmail.com>
2020-12-17 00:37:01 +0100

    add

---
+++ assets/js/date_heure.js
@@ -0,0 +1,35 @@
+function date_heure(id)
+{
+        date = new Date;
+        annee = date.getFullYear();
+        moi = date.getMonth();
+        mois = new Array('يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر');
+
+
+               
+
+       
+	   j = date.getDate();
+        jour = date.getDay();
+        jours = new Array('الحد', 'الاتنين', 'التلات', 'الاربع', 'الخميس', 'الجمعه', 'السبت');
+		 
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        resultat = ' '+jours[jour]+' '+j+' '+mois[moi]+' '+annee+' '+h+':'+m+':'+s;
+        document.getElementById(id).innerHTML = resultat;
+        setTimeout('date_heure("'+id+'");','1000');
+        return true;
+}
